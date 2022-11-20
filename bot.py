70643e1f7516138f34743a98b552658dc61dff13
rin-gil <91410075+rin-gil@users.noreply.github.com>
2022-11-20 21:25:51 +0200

    Remove Redis

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 from tgbot.config import load_config
 from tgbot.filters.admin import AdminFilter
@@ -33,16 +32,15 @@ def register_all_handlers(dp):
 async def main():
     logging.basicConfig(
         level=logging.INFO,
-        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
+        format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
     )
     logger.info("Starting bot")
     config = load_config(".env")
 
-    storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
-    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
-    dp = Dispatcher(bot, storage=storage)
+    bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
+    dp = Dispatcher(bot, storage=MemoryStorage())
 
-    bot['config'] = config
+    bot["config"] = config
 
     register_all_middlewares(dp, config)
     register_all_filters(dp)
@@ -57,7 +55,7 @@ async def main():
         await bot.session.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
