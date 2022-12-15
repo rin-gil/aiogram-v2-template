fcc1b97ba2497ca691c463478482bd71996f6c5d
rin-gil <91410075+rin-gil@users.noreply.github.com>
2022-12-16 00:39:39 +0200

    refactor: Change imports

--- tgbot/handlers/echo.py
+++ tgbot/handlers/echo.py
@@ -1,17 +1,19 @@
 """Handles echoes of the message"""
 
-from aiogram import types, Dispatcher
+from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
+from aiogram.types import ContentTypes, Message
+
 from aiogram.utils.markdown import hcode
 
 
-async def bot_echo(message: types.Message) -> None:
+async def bot_echo(message: Message) -> None:
     """Echoes on a message with no state"""
     text = ["Эхо без состояния.", "Сообщение:", message.text]
     await message.answer("\n".join(text))
 
 
-async def bot_echo_all(message: types.Message, state: FSMContext) -> None:
+async def bot_echo_all(message: Message, state: FSMContext) -> None:
     """Echoes on the message in state"""
     state_name = await state.get_state()
     text = [f"Эхо в состоянии {hcode(state_name)}", "Содержание сообщения:", hcode(message.text)]
@@ -21,4 +23,4 @@ async def bot_echo_all(message: types.Message, state: FSMContext) -> None:
 def register_echo(dp: Dispatcher) -> None:
     """Registers message handlers"""
     dp.register_message_handler(bot_echo)
-    dp.register_message_handler(bot_echo_all, state="*", content_types=types.ContentTypes.ANY)
+    dp.register_message_handler(bot_echo_all, state="*", content_types=ContentTypes.ANY)
